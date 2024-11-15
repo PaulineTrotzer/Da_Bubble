78778ef3b673f066627b8640f661b80f85963a24
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-11-15 20:25:47 +0100

    channel chat wip

--- src/app/channel-chat/channel-chat.component.ts
+++ src/app/channel-chat/channel-chat.component.ts
@@ -1,49 +1,78 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { collection, CollectionReference, DocumentData, Firestore, onSnapshot, orderBy, query } from '@angular/fire/firestore';
+import { Component, inject, Input, OnInit, SimpleChanges } from '@angular/core';
+import {
+  collection,
+  CollectionReference,
+  DocumentData,
+  Firestore,
+  onSnapshot,
+  orderBy,
+  query,
+} from '@angular/fire/firestore';
 import { GlobalVariableService } from '../services/global-variable.service';
 
 @Component({
   selector: 'app-channel-chat',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './channel-chat.component.html',
-  styleUrl: './channel-chat.component.scss'
+  styleUrl: './channel-chat.component.scss',
 })
 export class ChannelChatComponent implements OnInit {
-  constructor(){}
-
-  firestore = inject(Firestore)
-  global = inject(GlobalVariableService);
+  constructor() {}
 
   @Input() selectedChannel: any;
 
-  messagesData: any[] = []
+  firestore = inject(Firestore);
+  global = inject(GlobalVariableService);
+
+  messagesData: any[] = [];
   showThreadInfo: boolean = false;
+
+  unsubscribe: (() => void) | undefined;
 
   ngOnInit(): void {
     this.loadChannelMessages();
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedChannel'] && this.selectedChannel) {
+      this.loadChannelMessages();
+    }
+  }
+
   loadChannelMessages() {
     if (!this.selectedChannel) {
-       console.warn('No channel selected');
-       return;
+      console.warn('No channel selected');
+      return;
     }
- 
-    const messagesRef = collection(this.firestore, 'channels', this.selectedChannel.id, 'messages');
- 
+
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+
+    const messagesRef = collection(
+      this.firestore,
+      'channels',
+      this.selectedChannel.id,
+      'messages'
+    );
+
     const q = query(messagesRef, orderBy('timestamp', 'asc'));
     onSnapshot(q, (querySnapshot: any) => {
-       this.messagesData = querySnapshot.docs.map((doc: any) => {
-          const data = doc.data();
-          if (data.timestamp && data.timestamp.seconds) {
-             data.timestamp = new Date(data.timestamp.seconds * 1000);
-          }
-          return { id: doc.id, ...data };
-       });
+      this.messagesData = querySnapshot.docs.map((doc: any) => {
+        const data = doc.data();
+        if (data.timestamp && data.timestamp.seconds) {
+          data.timestamp = new Date(data.timestamp.seconds * 1000);
+        }
+        return { id: doc.id, ...data };
+      });
     });
- }
+  }
+
+  ngOnDestroy(): void {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
 }
