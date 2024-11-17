919e9a78c300f226876e1e650c5103613743aec1
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-11-17 20:07:26 +0100

    emoji picker working, add to firebase profile works

--- src/app/channel-chat/channel-chat.component.ts
+++ src/app/channel-chat/channel-chat.component.ts
@@ -1,16 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, OnInit, SimpleChanges } from '@angular/core';
 import {
+  addDoc,
   collection,
-  CollectionReference,
-  DocumentData,
+  doc,
+  DocumentReference,
   Firestore,
+  getDoc,
   onSnapshot,
   orderBy,
   query,
+  updateDoc,
 } from '@angular/fire/firestore';
 import { GlobalVariableService } from '../services/global-variable.service';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { getAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-channel-chat',
@@ -30,11 +34,15 @@ export class ChannelChatComponent implements OnInit {
   messagesData: any[] = [];
   showThreadInfo: boolean = false;
   hoveredMessageId: string | null = null;
+  isPickerVisible: string | null = null;
+  showReaction: boolean = true;
+  currentUserLastEmojis: string [] = [];
 
   unsubscribe: (() => void) | undefined;
 
   ngOnInit(): void {
     this.loadChannelMessages();
+    this.loadCurrentUserEmojis();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -75,6 +83,51 @@ export class ChannelChatComponent implements OnInit {
   ngOnDestroy(): void {
     if (this.unsubscribe) {
       this.unsubscribe();
+    }
+  }
+
+  addEmoji(event: any, messageId: string) {
+    const emoji = event.emoji.native;
+    this.isPickerVisible = null;
+    this.addLastUsedEmoji(emoji)
+  }
+
+  togglePicker(messageId: string) {
+    this.isPickerVisible = this.isPickerVisible === messageId ? null : messageId;
+  }
+
+  async addLastUsedEmoji(emoji: any) {
+    const auth = getAuth()
+    const currentUserId = auth.currentUser?.uid
+    if(currentUserId) {
+      const docRef = doc(this.firestore, 'users', currentUserId);
+      await updateDoc(docRef, {
+        lastEmojis: [emoji, ...(await this.getExistingEmojis(docRef))].slice(0, 2),
+      })
+    }
+  }
+
+  async getExistingEmojis(userDocRef: DocumentReference): Promise<string[]> {
+    const userDoc = await getDoc(userDocRef);
+    const userData = userDoc.data();
+    return userData?.['lastEmojis'] || [];
+  }
+
+  loadCurrentUserEmojis() {
+    const auth = getAuth();
+    const currentUserId = auth.currentUser?.uid;
+  
+    if (currentUserId) {
+      const userDocRef = doc(this.firestore, 'users', currentUserId);
+  
+      onSnapshot(userDocRef, (docSnapshot) => {
+        const userData = docSnapshot.data();
+        if (userData?.['lastEmojis']) {
+          this.currentUserLastEmojis = userData['lastEmojis'];
+        }
+      });
+    } else {
+      console.warn('No current user logged in');
     }
   }
 }
