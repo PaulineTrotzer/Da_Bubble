9b3c6a9b7b74294a39f859b93fc3388a3e92034d
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-11-22 21:09:39 +0100

    reactions from, done

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -5,23 +5,27 @@ export class User {
     password: string;
     picture?: string;
     status: string;
-
-    constructor(obj?: any, uid?: string ) {
-        this.uid = uid || obj?.uid || ''; 
-        this.name = obj?.name || '';
-        this.email = obj?.email || '';
-        this.password = obj?.password || '';
-        this.picture = obj?.picture || 'assets/img/picture_frame.png';
-        this.status = obj?.status || '';
+    lastEmojis: string[]; // Add lastEmojis property
+  
+    constructor(obj?: any, uid?: string) {
+      this.uid = uid || obj?.uid || '';
+      this.name = obj?.name || '';
+      this.email = obj?.email || '';
+      this.password = obj?.password || '';
+      this.picture = obj?.picture || 'assets/img/picture_frame.png';
+      this.status = obj?.status || '';
+      this.lastEmojis = obj?.lastEmojis || ['😍', '😇'];
     }
-
+  
     public toJSON() {
-        return {
-            uid: this.uid,
-            name: this.name,
-            email: this.email,
-            picture: this.picture,
-            status: this.status
-        };
+      return {
+        uid: this.uid,
+        name: this.name,
+        email: this.email,
+        picture: this.picture,
+        status: this.status,
+        lastEmojis: this.lastEmojis,
+      };
     }
-}
+  }
+  
